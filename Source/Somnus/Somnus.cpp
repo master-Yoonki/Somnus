cd5857e29b6895adb64e05ebf0cd355c8e00893a
Yoonki Kim <kuk7803@gmail.com>
2026-03-03 04:52:35 +0900

    Initial commit: Project Somnus base setup with Git LFS

---
+++ Source/Somnus/Somnus.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Somnus.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Somnus, "Somnus" );
